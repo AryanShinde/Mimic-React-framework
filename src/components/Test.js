9651559510f687c9661b069df3f9e09f652a01f6
AryanShinde <aryan.shinde20@vit.edu>
2022-06-25 00:24:29 +0530

    qr code functionality added to the site

---
+++ src/components/Test.js
@@ -0,0 +1,4 @@
+const Test = () => {
+  return <h1>Helloooooo???</h1>;
+};
+export default Test;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,55 @@
 /** @jsxRuntime classic */
 // import React from "react";
 // import ReactDom from "react-dom";
-import Mimic from "./mimic";
-
+import Mimic from './mimic';
+import Test from './components/Test';
+import heroo from './images/heroo.png';
 /** @jsx Mimic.createElement */
+
 const element = () => {
-  const hell = "hee";
+  const hell = 'hee';
+
   return (
     <div>
-      <h1>helooo</h1>
-      <h2>its workinggg</h2>
-      <h4 style="background-color: red; color: white">{hell}</h4>
+      <nav style="display: flex; justify-content: space-between; align-items: center; padding:0.5rem 5rem; position:sticky; top:0%; background-color: white;box-shadow: 2px 2px 10px #bcc8df; margin:0rem;border-radius: 2rem; z-index:8">
+        <div style="font-size:4rem">🔮</div>
+        <div style="font-size: 1.6rem">Aryan Shinde - 24</div>
+      </nav>
+
+      <div style="display: flex; justify-content: space-between; align-items: center;height:80vh;margin:1rem 1.5rem">
+        <div
+          className="left"
+          style="width:45%; margin-left:3rem; margin-top:-4rem"
+        >
+          <h1 style="font-size:7rem;">Mimic.</h1>
+
+          <h4 style="color:#ea4c89; font-size:2rem; margin-top:-2rem; position:relative;">
+            What it does?
+          </h4>
+          <p style="font-size:1.2rem">
+            -Mimic is an under-work React framework which has capabilities to
+            write JSX without using React's render or createComponent.
+          </p>
+          <button style="background:#135ce7; border:0rem; border-radius:1rem; padding:1rem; font-size:1.2rem">
+            <a
+              style="text-decoration:none; color: white"
+              href="https://github.com/AryanShinde/Mimic-React-framework"
+            >
+              Check the Project!
+            </a>
+          </button>
+        </div>
+        <div className="right">
+          <img style="width:38rem; height:30rem" src={heroo} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
 // const test = document.createElement("h1");
 
 // console.log(createElement("h1", { title: "hello" }, "this is text"));
-const container = document.querySelector("#root");
+const container = document.querySelector('#root');
 // container.appendChild(test);
 console.log(container);
 
